5a4fcdfbc0f3568e3d8ee35b0443f0e1cf80a0b0
simonenapolitano <simone.napolitano.08@marconigorgonzola.it>
2026-01-15 14:58:49 +0100

    Iniziato esercizio laboratorio 15-01-26

---
+++ Censimento/Cittadino.java
@@ -0,0 +1,44 @@
+public class Cittadino{
+    private String nome;
+    private String cognome;
+    private int eta;
+    private String codiceFiscale;
+
+    public Cittadino(String nome, String cognome, int eta, String codiceFiscale){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.eta = eta;
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public String getCognome(){
+        return cognome;
+    }
+    public int getEta(){
+        return eta;
+    }
+    public String getCodiceFiscale(){
+        return codiceFiscale;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setCognome(String cognome){
+        this.cognome = cognome;
+    }
+    public void setEta(int eta){
+        this.eta = eta;
+    }
+    public void setCodiceFiscale(String codiceFiscale){
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    @Override
+    public String toString() {
+        
+        return "Nome " + nome + "   Cognome " + cognome;
+    }
+}
---
+++ Censimento/UfficioAnagrafe.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UfficioAnagrafe{
+    ArrayList<Cittadino> registro = new ArrayList<Cittadino>();
+
+    public UfficioAnagrafe(){
+        Scanner scanner = new Scanner(System.in);
+        int scelta;
+        registro.add(new Cittadino("Mario", "Rossi", 20, "1"));
+        registro.add(new Cittadino("Luca", "Verdi", 19, "2"));
+        registro.add(new Cittadino("Marco", "Gialli", 17, "3"));
+        registro.add(new Cittadino("Andrea", "Di Santo", 25, "4"));
+        do{
+            for(int i=0; i < registro.size(); i++){
+                System.out.println(String.valueOf((i+1)) + " " + registro.get(i));
+            }
+            System.out.println("\n[1]VISUALIZZA I DETTAGLI COMPLETI\n[2]ESCI DAL PROGRAMMA");
+            try {
+                scelta = scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Devi inserire un numero!");
+                continue;
+            }
+            switch(scelta){
+                case 1:
+                    do{
+                        try {
+                            System.out.println("Inserisci il numero del cittadino che vuoi vedere: ");
+                            scelta-=1;
+                            try {
+                                System.out.println(registro.get(scelta) + "    " +  registro.get(scelta).getEta() + "    " + registro.get(scelta).getCodiceFiscale());
+                                scelta = scanner.nextInt();
+                                throw new IndexOutOfBoundsException("Cittadino " + scelta + " non esistente!");
+                            } catch (IndexOutOfBoundsException e) {
+                                System.out.println(e.getMessage());
+                                continue;
+                            }
+                        } catch (InputMismatchException e) {
+                            System.out.println("Devi inserire un numero!");
+                            continue;
+                        }
+                    }while(scelta <= 0 || scelta > registro.size());
+                    
+                    break;
+                case 2:
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Scelta [" + scelta + "] non valida!");
+            }
+
+            
+        }while(true);
+        
+        
+    }
+
+    public static void main(String[] args) {
+        new UfficioAnagrafe();
+    }
+}
